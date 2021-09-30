a3a14d7e509fe8341c840db4ee2b5a7b6d452444
michael steinacher <steinacherm@hotmail.co.uk>
2021-09-30 14:24:02 +0100

    Initial commit with completed name game

---
+++ name-game.rb
@@ -0,0 +1,18 @@
+#The user sees a greeting, which asks them to enter their name.
+#The user enters their name.
+#If the user's name begins with 'S', the program shouts the user's name back at them.
+#If the user's name begins with any other letter, the program just says 'Hi, ' plus their name.
+
+puts "Please enter your name"
+
+user_input = gets.chomp.capitalize
+
+original_input = user_input
+
+user_input = user_input.chars
+
+if user_input[0] == "S"
+  puts original_input.upcase
+else
+  puts original_input
+end
